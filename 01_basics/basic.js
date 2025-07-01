54c874c8af652f39a3e2cd76a0eaf798cfd1c2d8
Shrey <116195641+shrey1184@users.noreply.github.com>
2025-07-01 10:42:17 +0000

    notes on variables and datatypes

---
+++ 01_basics/basic.js
@@ -0,0 +1,6 @@
+const accountId = 144553
+var accountEmail = "Shreyshrivastava11@gmail.com"
+let accountCity = "Jaipur"
+accountPassword = "123456"
+
+console.table([accountId, accountCity, accountEmail, accountPassword]);
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+//alert(3+3)we are using nodejs not browser
+// high redeabilty
+// tc39 mdn = documentation
+let name = "hitesh"
+let age = 16
+let isLoggedIn = false
+
+// number = 2^53
+//bigint
+//string = ''
+//boolean = true/false
+//null = standalone value - object
+//undefined = undefined
+//symbol
+//object
+
+console.log(typeof null)
